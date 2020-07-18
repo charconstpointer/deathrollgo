39aa0d7a185635adce12e68fd3b3524b2bb3c467
charconstpointer <attwistingnether@gmail.com>
2020-07-18 17:55:18 +0200

    port flag

--- cmd/dr/dr.go
+++ cmd/dr/dr.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"net"
+	"strconv"
+	"strings"
 
 	"github.com/charconstpointer/deathrollgo/pkg/game/api"
 
@@ -11,8 +15,12 @@ import (
 
 func main() {
 	log.SetFormatter(&log.JSONFormatter{})
+	addr, err := parseAddr()
+	if err != nil {
+		addr = "0.0.0.0:8080"
+	}
 
-	lis, err := net.Listen("tcp", "0.0.0.0:8080")
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Errorf("failed to listen: %v", err)
 	}
@@ -21,4 +29,18 @@ func main() {
 
 	api.RegisterGameServiceServer(gs, s)
 	gs.Serve(lis)
+}
+
+func parseAddr() (string, error) {
+	port := flag.Int("port", 8080, "port you want to listen on for incoming grpc requests")
+	flag.Parse()
+
+	if *port < 1024 || *port > 49151 {
+		return "", errors.New("port out of range")
+	}
+	sb := strings.Builder{}
+	sb.WriteString("0.0.0.0")
+	sb.WriteString(":")
+	sb.WriteString(strconv.Itoa(*port))
+	return sb.String(), nil
 }
